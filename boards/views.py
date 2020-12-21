050435150265928d9187756975fc4df1ef4d1711
Abrahamsiasy <abrahamsisaysis@gmail.com>
2020-12-21 13:37:07 +0300

    A Complete Beginner's Guide to Django - Part 2

--- boards/views.py
+++ boards/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
+
+
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import Board
+
 
 def home(request):
-	return HttpResponse('HELLO WORLD!')
+	boards = Board.objects.all()
+	return render(request, 'home.html', {'boards': boards})
+	
+		
